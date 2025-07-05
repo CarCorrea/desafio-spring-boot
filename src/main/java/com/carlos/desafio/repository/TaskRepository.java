0481a2a1f5ae5074db078883e75023658fed33b5
Carlos Correa <carlos.correa.zapata@gmail.com>
2025-07-05 06:10:37 -0400

    Started technical challenge project for HF solutions softwarre developer application, created openapi.yml with the API's template using API first methodology

---
+++ src/main/java/com/carlos/desafio/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.carlos.desafio.repository;
+
+import com.nuevospa.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
